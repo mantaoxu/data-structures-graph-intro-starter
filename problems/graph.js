e1918c88cb0d3448cdfab1c67e2d146c9f45fe8c
username <mantaoxu2@gmail.com>
2021-05-26 13:42:22 -0700

    finished another method

--- problems/graph.js
+++ problems/graph.js
@@ -9,38 +9,44 @@ class Graph {
     if(!this.adjList[vertex]) {
       this.adjList[vertex] = []; // we are making a key - value pair in the object
     }
-
   }
 
   addEdges(edge1, edge2) {
-    // Code goes here ...
     // if no edge we add the edge into the graph
     this.addVertex(edge1);
     this.addVertex(edge2);
     this.adjList[edge1].push(edge2)
     this.adjList[edge2].push(edge1)
-
   }
 
   buildGraph(edgeList) {
-    // Code goes here ...
-
     edgeList.forEach((edge) => {
-
       if(edge.length === 1){
         this.addVertex(edge[0])
       } else{
       this.addEdges(edge[0], edge[1])
       }
     })
-
-    return this.adjList
-
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
-
+    let queue = [startingVertex];   // starting point 
+    let finalArr = [];
+    let visited = {};
+    visited[startingVertex] = true;   // the first vertex is already been visited
+    let currVertex;    
+    while(queue.length){ // if the queue has something(vertex) inside
+      currVertex = queue.shift();   // the first vertex shift from the queue
+      finalArr.push(currVertex);   // push this current vertex into the finalArr
+      this.adjList[currVertex].forEach(neighbor => { // 
+        if(!visited[neighbor]){  // if the neighbor has not been visited, then 
+          visited[neighbor] = true; // this become true
+          queue.push(neighbor); // push neighbor into queue;
+        }
+      })
+    }
+    return finalArr;
   }
 
   depthFirstTraversalIterative(startingVertex) {
