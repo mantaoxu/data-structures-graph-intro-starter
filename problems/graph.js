faa19e848c35ba510559c69b631a77338736e31c
Jun <luo.jnny@gmail.com>
2021-05-26 10:53:05 -0700

    addEdges completed

--- problems/graph.js
+++ problems/graph.js
@@ -2,18 +2,24 @@
 class Graph {
   constructor() {
     this.adjList = {};
-    
+
   }
 
-  addVertex(vertex) {
+  addVertex(vertex) { //creating the first vertex(node) in the graph. a helper function
     if(!this.adjList[vertex]) {
-      this.adjList[vertex] = [];
+      this.adjList[vertex] = []; // we are making a key - value pair in the object
     }
-    
+
   }
 
-  addEdges(srcValue, destValue) {
+  addEdges(edge1, edge2) {
     // Code goes here ...
+    // if no edge we add the edge into the graph
+    this.addVertex(edge1);
+    this.addVertex(edge2);
+    this.adjList[edge1].push(edge2)
+    this.adjList[edge2].push(edge1)
+
   }
 
   buildGraph(edges) {
@@ -37,12 +43,3 @@ class Graph {
 module.exports = {
   Graph
 };
-
-
-
-
-
-
-
-
-
